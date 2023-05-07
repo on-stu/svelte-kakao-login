fc475a295d016228b7998bd15064162fe46c8a38
on-stu <minsu0523@naver.com>
2023-05-07 11:39:18 +0900

    FEAT : kakao login button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	function onKakaoLogin() {
+		// 카카오 로그인 버튼을 눌렀을 때 동작하는 함수
+	}
+</script>
+
+<button on:click={onKakaoLogin}>
+	<img
+		src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+		width="222"
+		alt="카카오 로그인 버튼"
+	/>
+</button>
